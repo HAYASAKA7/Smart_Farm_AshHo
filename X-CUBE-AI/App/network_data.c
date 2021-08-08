d7f863faaf60202ce1a82bec883d1358a9d6e6d9
HAYASAKA7 <cyanluxury267@gmail.com>
2021-08-08 10:54:36 +0800

    first try

---
+++ X-CUBE-AI/App/network_data.c
@@ -0,0 +1,159 @@
+/**
+  ******************************************************************************
+  * @file    network_data.c
+  * @author  AST Embedded Analytics Research Platform
+  * @date    Wed Aug  4 15:31:22 2021
+  * @brief   AI Tool Automatic Code Generator for Embedded NN computing
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2021 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software component is licensed by ST under Ultimate Liberty license
+  * SLA0044, the "License"; You may not use this file except in compliance with
+  * the License. You may obtain a copy of the License at:
+  *                             www.st.com/SLA0044
+  *
+  ******************************************************************************
+  */
+#include "network_data.h"
+#include "ai_platform_interface.h"
+
+
+AI_ALIGNED(32)
+const ai_u64 s_network_weights_array_u64[ 312 ] = {
+  0xbf5ef8fdbfacbde8U, 0xbf90d182bf8af83dU, 0xbfcac441bf0276bbU, 0xbee705efbe917518U,
+  0x3fb3a6c9bfd12e92U, 0x3f48b18a3f97b7a2U, 0x3f7a46a73fabc13eU, 0x3f92d5ea3edddc7eU,
+  0x3dd85ce33f48af2cU, 0x3f3ae9b43f32fae2U, 0x3f12fe363f9b23f4U, 0x3fd7da403f95b6f5U,
+  0x3fab443e4005fea2U, 0xbf9127b03fdab8f0U, 0xbfa2ab4dbf953a76U, 0xbf658f29bfa735c4U,
+  0xbf5cf1a3bf698f14U, 0xbf59c5e0bf69193aU, 0x3de2dd95bd5651a1U, 0x3e8e4894be5b31bcU,
+  0x3f3bae6dbf8350fbU, 0xbf4bda643ee1c7e5U, 0x3f006fda3e142ce3U, 0xbe87b4283f39ab41U,
+  0x3e0c2695bdd0eec7U, 0x3e9e65013edaffbeU, 0x3f0ef054bd6af031U, 0xbe953c943ea803cbU,
+  0xbcf8d0f7bec49df9U, 0x3e0c8a013ecc3d09U, 0x3ed4c7aabed4d7d6U, 0xbdad50283e373fffU,
+  0xbea192493f0ef9a5U, 0x3f5ff84dbf2c9466U, 0xbed9b42e3ee2974bU, 0x3fb74ff2bf8680dbU,
+  0xbf88b2813f0f8506U, 0x3fc7abafbf96b220U, 0xbf55d3ba3f26f0c2U, 0x3e453f27bf37e7a4U,
+  0xbf467270be304bcfU, 0x3e95e23abe81966bU, 0xbdba4f59bf61ac8cU, 0xbf095e283f1ba711U,
+  0xbfa1ce8a3f2327b6U, 0x3f1e0afdbf889236U, 0xbf04bec6be9fe52fU, 0xbe1bef0cbe25d372U,
+  0x3ebb0f7abf764874U, 0xbf9d06c83e793d23U, 0xbe024835bdf5c848U, 0x3f1e3ca63ec5c035U,
+  0x3f2022c3be94bba8U, 0xbf35dd503eddfe9cU, 0x3ed779e3bf3d4309U, 0xbf2fbed83ecfe09cU,
+  0xbd86523c3e992f7eU, 0xbe81aeb7beda7ad4U, 0xbe4e438ebedb9d65U, 0x3e9763723d941c8dU,
+  0x3f165bb0bf46aadcU, 0xbde6d5a6bf7ccf53U, 0x3db5ff13bf029b5bU, 0xbda9c9f53deabd76U,
+  0x3d719d4fbe8dbb20U, 0x3e46e24c3dca7366U, 0xbe1faebbbf01757aU, 0x3e86e875bf823bb3U,
+  0xbc837b32be254453U, 0xbf13bc9c3ea0c7b2U, 0x3e4b53eebe1d2895U, 0x3f1fdd973c483132U,
+  0x3e89fe04bf6982dfU, 0x3ce11ee2bec7f5acU, 0xbf0601ccbe6bbb39U, 0x3f083a28bf24009aU,
+  0x3e9b1b91be8259b4U, 0x3cbce034becbb5ecU, 0x3e825cfe3ce4e276U, 0x3dfa0f3ebd9f8c46U,
+  0x3b9968e93ce6071aU, 0x3e9faca6be5139a7U, 0xbeca11a53dbbea6aU, 0x3ca623d4bf147662U,
+  0xbefbf49fbe99886fU, 0x3d603e0abdf10050U, 0x3e7d248cbe785b01U, 0xbf1afd343e476035U,
+  0xbdc730b2bef7e75aU, 0x3f1dfe2bbe7cbca2U, 0xbd072318bd210b61U, 0x3d946c89bf125ff6U,
+  0xbe8b00363b1766afU, 0xbec576be3ee83babU, 0x3f25de45be4e4620U, 0xbe8726133e28fc02U,
+  0xbf098dadbf1aae81U, 0xbdd52634bdaea12eU, 0x3e4c9d143d221a87U, 0x3f124ed23cee68d8U,
+  0xbed357153e55940bU, 0x3cbd0bbfbf51fc5eU, 0xbaee7058bf231026U, 0xbe8325453da0a6b3U,
+  0x3e9e53f23e02c209U, 0x3c938c60bca38aa8U, 0xbebfd755be18ac4bU, 0xbcc726e9be9636d9U,
+  0xbce4ce183e34b1c3U, 0xbd9be0e73e2e7d06U, 0xbea31ee1be9c67cbU, 0xbf3d4711bf655b1dU,
+  0xbee6f5bbbe9fc3c2U, 0xbf06cffc3f02cfe3U, 0xbeb9d7f73e26be6dU, 0xbea9b3543dc8f0cdU,
+  0xbe8685b0bdbb310bU, 0xbd9f589b3e5918bcU, 0xbf1522d73e55b776U, 0xbea8c139bea93768U,
+  0x3cfec5d3be671541U, 0xbcc8bed53d85ccceU, 0x3e9f1e60be745154U, 0x3f06946c3c7018e8U,
+  0x3e071b0c3ccf53c0U, 0xbec58fbe3e30e3a8U, 0xba8a59a4be55e2f1U, 0x3eb0780a3e9ee341U,
+  0x3d2d8ab63e2e8a66U, 0xbeeb115d3e0f1780U, 0xbf044d733e98f54dU, 0x3ed39c9a3edf43e3U,
+  0x3ccf9b1d3e8decd2U, 0x3eddf9543e118f57U, 0x3d3fc790beb5796cU, 0xbe9d099dbf14a692U,
+  0xbf47952ebf1b199eU, 0xbe878d14bd739b87U, 0xbecde9573ebaf4e0U, 0xbd1e4fc9bebcf90aU,
+  0x3e7a7741bea366c1U, 0xbe34faa63edaf30cU, 0xbec05b7f3e13faf5U, 0xbd9ac4fcba97b1b2U,
+  0x3e58ca89be1c328fU, 0xbea932fc3d04872eU, 0xbf2fe7a83e1cad14U, 0xbf079360be34e7a6U,
+  0xbf4b54303dee65a1U, 0xbf2b0bbf3e7302e2U, 0x3ea8c571be2a0e9bU, 0xbe0b10e23ea3fa4dU,
+  0x3f244a993f1be2f1U, 0x3e48650a3dc2889fU, 0x3e0f3153be13f79aU, 0x3e55a8ac3bdb89b5U,
+  0xba8b4a4a3e2a202cU, 0x3e96611fbe84ebc1U, 0xbe866e91bf65b26bU, 0xbf0df820bf416215U,
+  0xbe15a2b6bed15a31U, 0x3e498b663d3cbfafU, 0xbe5dcbe8bf320d15U, 0x3d5956a1beb45a26U,
+  0xbe7dfcdebeeb56c3U, 0x3f15fc18beb68eadU, 0x3ebe2e3fbdf21c31U, 0x3d60c1abbe5878e7U,
+  0x3e509b5cbd14c4b1U, 0xbe1182483d23f521U, 0x3e01aeeabe4c908dU, 0x3e8260afbf224ee8U,
+  0xbd7f95f43eaad069U, 0x3e7246fe3db1b1bbU, 0x3df7bb783e937aa7U, 0xbde844bf3f037a8fU,
+  0xbeb5790c3e48d86aU, 0xbf39667dbc88c227U, 0xbf1b050fbdd026c1U, 0xbcaf2ae53ebc135bU,
+  0x3d69d464be2fbe5aU, 0xbe54d7573c06f5aeU, 0x3e90fd4abf172fc3U, 0xbde6f689bf3ca2aeU,
+  0xbf001a363c96c3efU, 0xbd7e1d8e3ee63852U, 0x3eb8caa43b11b18bU, 0x3d07682fbee95d76U,
+  0x3de217aebf0f9050U, 0xbe4d9f493cecd7d5U, 0xbf0606973f1960bbU, 0x3f162f333e9e31d6U,
+  0xbe39d051be3a8e43U, 0xbd6b32b1bdcc71a0U, 0xbee023b0bcd8a467U, 0xbf53a2fa3ec76e37U,
+  0xbfa35a213d15c060U, 0xbe55f2b03e054ab9U, 0xbef84c19bdf7fbaaU, 0xbe264b023ed594a0U,
+  0x3de5cd073d7f9116U, 0xbd0403c03df1d77aU, 0x3d3a0be63d02c9a8U, 0x3cbca6293eb459b1U,
+  0x3e228e3f3e34679dU, 0x3e07e60b3e00e99dU, 0x3ec2c243be527d57U, 0xbc47903dbe5e5a85U,
+  0xbe361d11bdba0e5fU, 0xbe13ecad3db1c029U, 0x3dd544093f21b195U, 0x3dfe587c3f89a118U,
+  0x3eb4b1c7bf3cb97cU, 0x3e21b35ebe76f580U, 0xbdd2a1b8be084bcdU, 0x3ba5f77cbcd42363U,
+  0x3ee4a5f4bef75d94U, 0x3e29950cbfa55d95U, 0x3ea3ace9be369e9bU, 0x3e0db456bda89c01U,
+  0xbd9f8e33bda6c388U, 0xbe2cad5cbeea1d5dU, 0x3ea4a352be9bbb29U, 0x3e63d01ebf1ef502U,
+  0xbdd556c5be41885dU, 0xbd5dbbebbe46646bU, 0x3dcf2daa3e02301bU, 0xbf2fbce9bf3a27fcU,
+  0xbe7d0911be16680dU, 0xbeef268e3e7e844eU, 0xbeaafae73e4efe80U, 0xbe8909e83e9d6b24U,
+  0xbd1b1f8ebf115295U, 0xbe8fdf59bf3abde8U, 0xbf418554bf5bc42eU, 0xbe4acf0fbf1f3584U,
+  0xbd82333dbf233f45U, 0xbe1f19abbcfe57feU, 0xbe1539ad3e2c1b15U, 0x3dc1bb1f3f325c72U,
+  0xbebd72b8be185ba9U, 0xbe5e583a3e9735f9U, 0xbea2141e3bccd50dU, 0xbe68fb113e958faaU,
+  0xbd88f87b3f2b12b6U, 0x3de26d793e6806fcU, 0xbe1672c53da7c890U, 0xbddea2c33eea265cU,
+  0x3e56fbbe3eba9713U, 0x3e0f56c1bef2fcb6U, 0x3e41e3fc3d8af64bU, 0xbdf6ba913ef071eaU,
+  0xbe7ad461be2f786eU, 0xbd11832fbe95b79fU, 0xbe23ee6cbd936da1U, 0xbe955cdabcbd383bU,
+  0xbf8a4dbfbf131b37U, 0x3eb58a27bd8bcc01U, 0x3eb83f05bdd7d817U, 0x3f1312dfbd9e873fU,
+  0x3dcf2260be02b575U, 0xbe283088be14bdddU, 0x3ea063eabebf132bU, 0x3e414686bf2b1f9cU,
+  0xbf113bcbbf35bc3dU, 0xbd45a9dabe4018deU, 0x3eb97d973d42ddeeU, 0x3daa40b03e6c1d8dU,
+  0x3e17e3633dbf6976U, 0xbdea619dbf143cbfU, 0xbe87260abdf8c746U, 0x3e8333e43eb872d7U,
+  0x3e82e042bd06cba4U, 0x3f2eae5cbf492e9bU, 0xbda40786beb45c66U, 0xbee5367f3e84c04fU,
+  0xbefc13b4bdbe1c01U, 0x3d6fb88bbd06b85eU, 0x3e7791e5bc9fdea2U, 0xbe588375bf401ad4U,
+  0x3e6f48bdbeefa422U, 0x3e82d0563df82766U, 0xbe89c3ca3e4dab0fU, 0xbb226f013e1f892eU,
+  0x3f2f4dbfbfa1aca6U, 0x3d6ec9efbfbcf98eU, 0xbf1a7249bfd31ccaU, 0x3dd931f2bedecf14U,
+  0x3e8fa9013b7c18e2U, 0x3de861f3beb6864cU, 0xbef17e36bdb60585U, 0xbedd6986bce324cbU,
+  0x3ea272dbbf262ec1U, 0xbdd38687bdbfaa3aU, 0xbe753e203c7a2a3bU, 0x3dd220dbbe767848U,
+  0xbdfcc0253f2cef5dU, 0xbeb54718be0f7856U, 0xbf072e623ea7325dU, 0xbe86d01c3dfa153eU,
+  0xbd168a493eacfcefU, 0xbf0d97863d1b2e78U, 0xbe9b0c42be449ea5U, 0xbd57b631bd7eb94fU,
+  0xbe47bdf43ec575c9U, 0x3ce545423d18903eU, 0x3e38e063bf1c58dfU, 0xbc7982543ccd3e82U,
+  0xbcd84486be37016fU, 0x3c150fb93baacaecU, 0xbe0d0eb4be0c6279U, 0xbd5c8add3d9ee3ceU,
+};
+
+
+
+AI_API_DECLARE_BEGIN
+
+/*!
+ * @brief Get network weights array pointer as a handle ptr.
+ * @ingroup network_data
+ * @return a ai_handle pointer to the weights array
+ */
+AI_DEPRECATED
+AI_API_ENTRY
+ai_handle ai_network_data_weights_get(void)
+{
+  static const ai_u8* const s_network_weights_map[1 + 2] = {
+    AI_PTR(AI_MAGIC_MARKER),
+    AI_PTR(s_network_weights_array_u64),
+    AI_PTR(AI_MAGIC_MARKER)
+  };
+
+  return AI_HANDLE_PTR(s_network_weights_map);
+
+}
+
+
+/*!
+ * @brief Get network params configuration data structure.
+ * @ingroup network_data
+ * @return true if a valid configuration is present, false otherwise
+ */
+AI_API_ENTRY
+ai_bool ai_network_data_params_get(ai_handle network, ai_network_params* params)
+{
+  if (!(network && params)) return false;
+  
+  static ai_buffer s_network_data_map_activations[AI_NETWORK_DATA_ACTIVATIONS_COUNT] = {
+    AI_BUFFER_OBJ_INIT(AI_BUFFER_FORMAT_U8, 1, 1, AI_NETWORK_DATA_ACTIVATIONS_SIZE, 1, NULL)
+  };
+
+  const ai_buffer_array map_activations = 
+    AI_BUFFER_ARRAY_OBJ_INIT(AI_FLAG_NONE, AI_NETWORK_DATA_ACTIVATIONS_COUNT, s_network_data_map_activations);
+  
+  static ai_buffer s_network_data_map_weights[AI_NETWORK_DATA_WEIGHTS_COUNT] = {
+    AI_BUFFER_OBJ_INIT(AI_BUFFER_FORMAT_U8, 1, 1, 2496, 1, s_network_weights_array_u64),
+    
+  };
+
+  const ai_buffer_array map_weights = 
+    AI_BUFFER_ARRAY_OBJ_INIT(AI_FLAG_NONE, AI_NETWORK_DATA_WEIGHTS_COUNT, s_network_data_map_weights);
+
+  return ai_platform_bind_network_params(network, params, &map_weights, &map_activations);
+}
+
+
+AI_API_DECLARE_END
